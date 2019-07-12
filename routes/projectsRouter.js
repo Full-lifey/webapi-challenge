d6869a85096c5a5b959c1a2b793613a764b80c92
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-12 10:09:13 -0600

    moved validateProject and validateProjectId middleware functions to their own directory structure

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 const Projects = require('../data/helpers/projectModel.js');
+const validateProjectId = require('../middleware/validateProjectId.js');
+const validateProject = require('../middleware/validateProject.js');
 
 router.get('/', (req, res) => {
   Projects.get()
@@ -74,28 +76,28 @@ router.delete('/:id', validateProjectId, (req, res) => {
     });
 });
 
-function validateProjectId(req, res, next) {
-  const { id } = req.params;
-  Projects.get(id)
-    .then(project => {
-      if (project) {
-        req.project = project;
-        next();
-      } else {
-        res.status(400).json({ message: 'invalid project ID' });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Server error, unable to retrieve' });
-    });
-}
+// function validateProjectId(req, res, next) {
+//   const { id } = req.params;
+//   Projects.get(id)
+//     .then(project => {
+//       if (project) {
+//         req.project = project;
+//         next();
+//       } else {
+//         res.status(400).json({ message: 'invalid project ID' });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Server error, unable to retrieve' });
+//     });
+// }
 
-function validateProject(req, res, next) {
-  if (!req.body.name || !req.body.description) {
-    res.status(400).json({ message: 'name and description are required' });
-  } else {
-    next();
-  }
-}
+// function validateProject(req, res, next) {
+//   if (!req.body.name || !req.body.description) {
+//     res.status(400).json({ message: 'name and description are required' });
+//   } else {
+//     next();
+//   }
+// }
 
 module.exports = router;
